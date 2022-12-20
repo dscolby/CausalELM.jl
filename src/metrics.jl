f83529fe442fe87cb818bf90e5ae955ab4be6a77
dscolby <dscolby17@gmail.com>
2022-12-20 17:41:04 -0600

    Updated tolerance for least squares solution

--- src/metrics.jl
+++ src/metrics.jl
@@ -11,4 +11,8 @@ function mae(y_actual, y_pred)
     @assert length(y_actual) == length(y_pred) "y_actual and y_pred must be the same length"
     return @fastmath sum(abs.(y_actual .- y_pred)) / length(y_actual)
 end
+
+function confusion_matrix(y_actual, y_pred, threshold=0.5)
+    
+end
 end
