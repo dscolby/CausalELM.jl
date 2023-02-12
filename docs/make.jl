0ccc3e1f3ec7d1e7c5367ad81840bdf5c9391e5c
Darren Colby <dscolby17@gmail.com>
2023-02-12 16:35:26 -0600

    Changed master to main

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(;
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://dscolby.github.io/CausalELM.jl",
-        edit_link="master",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
@@ -21,5 +21,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/dscolby/CausalELM.jl",
-    devbranch="master",
+    devbranch="main",
 )
