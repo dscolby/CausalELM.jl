b27918c041043f69406becc10b07c1c00186a76e
Darren Colby <dscolby17@gmail.com>
2023-04-13 22:46:02 -0500

    Added assets parameter

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,13 @@
 using CausalELM
 using Documenter
+using DocumenterTools: Themes
 
+Themes.compile(joinpath(@__DIR__,"src/assets/dark.scss"), joinpath(@__DIR__,"src/assets/themes/documenter-dark.css"))
 DocMeta.setdocmeta!(CausalELM, :DocTestSetup, :(using CausalELM); recursive=true)
 
 makedocs(;
     modules=[CausalELM],
+    assets=["assets/favicon.ico", asset("https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap", class=:css)]
     authors="Darren Colby <dscolby17@gmail.com> and contributors",
     repo="https://github.com/dscolby/CausalELM.jl/blob/{commit}{path}#{line}",
     sitename="CausalELM.jl",
